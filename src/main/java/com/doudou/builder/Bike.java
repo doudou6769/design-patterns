a3e73660e0011f8ff0a459b5f2af3f34ec5000c8
doudou <13653506769@163.com>
2021-12-19 21:30:21 +0800

    feat：建造者模式

---
+++ src/main/java/com/doudou/builder/Bike.java
@@ -0,0 +1,31 @@
+package com.doudou.builder;
+
+/**
+ * <pre>
+ * 说   明：具体产品 自行车
+ * 创   建：窦慧文
+ * 日   期：2021/12/19
+ * Q    Q：1443502059
+ * </pre>
+ */
+public class Bike {
+
+    private String frame;   // 车架
+    private String seat;    // 车座
+
+    public String getFrame() {
+        return frame;
+    }
+
+    public void setFrame(String frame) {
+        this.frame = frame;
+    }
+
+    public String getSeat() {
+        return seat;
+    }
+
+    public void setSeat(String seat) {
+        this.seat = seat;
+    }
+}
---
+++ src/main/java/com/doudou/builder/HelloBikeBuilder.java
@@ -0,0 +1,26 @@
+package com.doudou.builder;
+
+/**
+ * <pre>
+ * 说   明：产品具体的构建者 哈啰单车构建者
+ * 创   建：窦慧文
+ * 日   期：2021/12/19
+ * Q    Q：1443502059
+ * </pre>
+ */
+public class HelloBikeBuilder extends BikeBuilder{
+    @Override
+    public void buildFrame() {
+        bike.setFrame("哈啰单车车架 - 蓝色车架");
+    }
+
+    @Override
+    public void buildSeat() {
+        bike.setSeat("哈啰单车车座 - 黑色车座");
+    }
+
+    @Override
+    public Bike createBike() {
+        return bike;
+    }
+}

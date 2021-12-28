d5510e59ca6ba097260b04652b3d5b4cb5e2834c
doudou <13653506769@163.com>
2021-12-28 20:31:58 +0800

    feat：外观模式

---
+++ src/main/java/com/doudou/facade/Light.java
@@ -0,0 +1,21 @@
+package com.doudou.facade;
+
+/**
+ * <pre>
+ * 说   明：电灯（子系统）
+ * 创   建：窦慧文
+ * 日   期：2021/12/28
+ * Q    Q：1443502059
+ * </pre>
+ */
+public class Light {
+
+    public void on(){
+        System.out.println("开灯");
+    }
+
+    public void off(){
+        System.out.println("关灯");
+    }
+
+}
---
+++ src/main/java/com/doudou/facade/Main.java
@@ -0,0 +1,18 @@
+package com.doudou.facade;
+
+/**
+ * <pre>
+ * 说   明：外观模式
+ * 创   建：窦慧文
+ * 日   期：2021/12/28
+ * Q    Q：1443502059
+ * </pre>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        SmartAppliancesFacade facade = new SmartAppliancesFacade();
+        facade.say("打开");
+    }
+
+}
